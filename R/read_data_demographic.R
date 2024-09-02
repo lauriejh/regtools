1901715ec49e6e6270f6edb620f14b2d06da56b1
Alejandra Martinez Sanchez <alejama@student.sv.uio.no>
2024-09-02 14:56:29 +0200

    Changed documentation and var names

--- R/read_data_demographic.R
+++ R/read_data_demographic.R
@@ -5,7 +5,7 @@
 #'`read_data_demographic()` validates the general structure and minimum column requirements for demographic data.
 #' The input data sets must be CSV, RDS, RDA or .SAV files.
 #' @param file_path File path of the demographic data to be read. Supports CSV, RDS, RDA and .SAV files.
-#' @param data_type Demographic data can either be of type "t_unvarying" or "t_varying", necessary to check correct data structure characteristics.
+#' @param data_type Demographic data can either be of type "t_variant" or "t_invariant", necessary to check correct data structure characteristics.
 #' @param id_col Name of ID column in data set, default is "id"
 #' @param date_col Name of date column in data set, default is "date"
 #' @param ... Optional extra parameters for specifying correct reading of CSV and .SAV files
@@ -26,7 +26,7 @@ read_demo_data <- function(file_path, data_type, id_col = "id", date_col = "date
   file_extension <- tolower(tools::file_ext(file_path))
   supported_types <- c("csv", "rds", "rda", "sav")
 
-  ###### Check file existance and type #####
+  ###### Check file existence and type #####
   stopifnot("File does not exist in the specified path." = file.exists(file_path))
 
   stopifnot("File type not supported. Please provide a .csv, .rds, or .sav file." = file_extension %in% supported_types)
@@ -61,9 +61,9 @@ read_demo_data <- function(file_path, data_type, id_col = "id", date_col = "date
   message("ID \u2713")
   Sys.sleep(1)
 
-  ###### Time dependent: Check date column #####
-  if (data_type == "time_dependent"){
-    message("Data type: time-dependent. Checking requirements...")
+  ###### Time variant: Check date column #####
+  if (data_type == "t_variant"){
+    message("Data type: time variant. Checking requirements...")
     date_column <- which(names(data) == date_col)
     if (length(date_column) == 0) {
       stop(paste0("The dataset must contain a column named: ", date_col))
@@ -75,9 +75,9 @@ read_demo_data <- function(file_path, data_type, id_col = "id", date_col = "date
     message("Date \u2713")}
   Sys.sleep(1)
 
-  ###### Persistent: check ID duplicates #####
-  if (data_type == "persistent"){
-    message("Data type: persistent. Checking requirements...")
+  ###### Time invariant: check ID duplicates #####
+  if (data_type == "t_invariant"){
+    message("Data type: time invariant Checking requirements...")
     if (any(duplicated(data[[id_column]]))) {
       stop("The dataset contains duplicate IDs. Verify that this dataset only containts persistent characteristics.")
     }
