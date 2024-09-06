87f75e15513b63e013b023e19fb1829fddf2793f
Alejandra Martinez Sanchez <alejama@student.sv.uio.no>
2024-09-06 15:38:58 +0200

    Exp add check for NAs, redundant

--- R/filter_demo.R
+++ R/filter_demo.R
@@ -2,21 +2,35 @@
 #'
 #' @param data Data frame containing pre-processed demographic data
 #' @param filter_param Named list containing filtering parameters. The names in the list are the column names and the values are vectors of values to keep.
+#' @param rm_na Removes observations that have NA in the filtered columns.
 #'
 #' @return Filtered demographic dataframe containing only relevant observations based on the filtering parameters.
-#' @importFrom purrr reduce
 #' @importFrom dplyr filter
+#' @importFrom dplyr if_any
+#' @importFrom tidyselect all_of
+#' @importFrom purrr reduce
 #' @importFrom rlang sym
 #' @export
 #'
 
-filter_demo <- function(data, filter_param){
+filter_demo <- function(data, filter_param, rm_na = TRUE){
   stopifnot("Specified variables do not exist in your dataset." = names(filter_param) %in% colnames(data))
 
   ####Keep only observations that comply with filtering parameters####
-  filtered_data <- purrr::reduce(names(filter_param), function(data, col) {
-    data |>  dplyr::filter(!!rlang::sym(col) %in% filter_param[[col]])
-    }, .init = data)
+  filtered_data <- purrr::reduce(names(filter_param), function(df, col) {
+    df |>  dplyr::filter(!!rlang::sym(col) %in% filter_param[[col]])
+  }, .init = data)
 
-  ####need to give a summary of the things that changed, n cases and other####
+  ####Check for NAs####
+  if (rm_na == TRUE) {
+    message("Removing observations containing NAs")
+    cols <- names(filter_param)
+    filtered_data_na <- filtered_data |>
+      dplyr::filter(!dplyr::if_any(tidyselect::all_of(cols), ~ is.na(.)))
+  }
+  else{
+    message("Not removing NAs")
+    filtered_data_na <- filtered_data
+    }
+  return(filtered_data_na)
 }
