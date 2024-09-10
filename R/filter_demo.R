2b8b6d5fe4779bab6bf53df0c2a7d43818bca2f6
Alejandra Martinez Sanchez <alejama@student.sv.uio.no>
2024-09-10 14:14:51 +0200

    Add more filtering options for time-variant datasets (any and data_type)

--- R/filter_demo.R
+++ R/filter_demo.R
@@ -3,12 +3,13 @@
 #' @param data Data frame containing pre-processed demographic data
 #' @param filter_param Named list containing filtering parameters. The names in the list are the column names and the values are vectors of values to keep.
 #' @param rm_na Removes observations that have NA in the non-filtered columns.
+#' @param data_type Type of demographic data: "t_variant" or "t-variant"
+#' @param any Filtering option, any year. Default = FALSE
 #'
 #' @return Filtered demographic dataframe containing only relevant observations based on the filtering parameters.
 #' @importFrom dplyr filter
 #' @importFrom purrr reduce
 #' @importFrom rlang sym
-#' @importFrom tidyr drop_na
 #' @importFrom skimr skim
 #' @importFrom tidyselect everything
 #' @importFrom dplyr across
@@ -17,20 +18,47 @@
 #' @export
 #'
 
-filter_demo <- function(data, filter_param, rm_na = TRUE){
+filter_demo <- function(data, data_type, filter_param, id_col = "id", any = FALSE, rm_na = TRUE){
   stopifnot("Specified variables do not exist in your dataset." = names(filter_param) %in% colnames(data))
 
+  if(missing(data_type)) {
+    stop("Data type not specified")
+  }
   ####Keep only observations that comply with filtering parameters####
-  filtered_data <- purrr::reduce(names(filter_param), function(df, col) {
-    df |>  dplyr::filter(!!rlang::sym(col) %in% filter_param[[col]])
-  }, .init = data)
-  message(paste0("Original dataset had "), nrow(data), " observations.")
-  cat("\n")
-  message(paste0("\u2022", nrow(filtered_data), " observations fullfilled the selected filtering parameters: "))
-  Sys.sleep(2)
-  print(data |> skimr::skim())
-
-  ####Check for NAs nad remove if needed####
+  if(data_type == "t_invariant"){
+    message("Filtering time-invariant dataset...")
+    filtered_data <- purrr::reduce(names(filter_param), function(df, col) {
+      df |>  dplyr::filter(!!rlang::sym(col) %in% filter_param[[col]])
+    }, .init = data)
+    message(paste0("Original dataset had "), nrow(data), " observations.")
+    cat("\n")
+    message(paste0("\u2022", nrow(filtered_data), " observations fullfilled the selected filtering parameters: "))
+    Sys.sleep(2)
+    print(filtered_data |> skimr::skim())
+  }
+  else if (data_type == "t_variant"){
+    message("Filtering time-variant dataset...")
+    if(any == FALSE){
+      filtered_data <- purrr::reduce(names(filter_param), function(df, col) {
+        df |>  dplyr::filter(!!rlang::sym(col) %in% filter_param[[col]])
+      }, .init = data)
+      message(paste0("Original dataset had "), nrow(data), " observations.")
+      cat("\n")
+      message(paste0("\u2022", nrow(filtered_data), " observations fullfilled the selected filtering parameters: "))
+      Sys.sleep(2)
+      print(filtered_data |> skimr::skim())
+    }
+    else if (any == TRUE){
+      message("Any as filtering option:")
+      filtered_data <- purrr::reduce(names(filter_param), function(df, col) {
+        df |>
+          dplyr::group_by(!!rlang::sym(id_col)) |>
+          dplyr::filter(any(!!rlang::sym(col) %in% filter_param[[col]])) |>
+          dplyr::ungroup()
+      }, .init = data)
+      }
+    }
+  ####Check for NAs and remove if needed####
   if (rm_na == TRUE) {
     n_missing <- data |>
       dplyr::summarize(dplyr::across(tidyselect::everything(), ~ sum(is.na(.))))
