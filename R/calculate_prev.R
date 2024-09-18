82797d3dc50119418fb5e5586166c9a8210e3cdb
Alejandra Martinez Sanchez <alejama@student.sv.uio.no>
2024-09-18 15:44:17 +0200

    General calculate_prev function

---
+++ R/calculate_prev.R
@@ -0,0 +1,6 @@
+calculate_prev <- function(count_data, pop_data, joining_var, diag_count_col = "diag_count", pop_count_col = "pop_count", cumulative = TRUE){
+  if(cumulative)
+  prev_data <- count_data |>
+      left_join(pop_data, by = joining_var) |>
+      mutate(prev_rate = diag_count_col/pop_count_col)
+}
