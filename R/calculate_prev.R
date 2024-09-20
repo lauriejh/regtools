1467e4b9b27eb1128ba8bfa463ddd1fb324fe4df
Alejandra Martinez Sanchez <alejama@student.sv.uio.no>
2024-09-20 12:40:55 +0200

    change names

--- R/calculate_prev.R
+++ R/calculate_prev.R
@@ -1,4 +1,4 @@
-calculate_incidence <- function(count_data, pop_data, joining_var, diag_count_col = "diag_count", pop_count_col = "pop_count", cumulative = TRUE){
+calculate_prev <- function(count_data, pop_data, joining_var, diag_count_col = "diag_count", pop_count_col = "pop_count", cumulative = TRUE){
   if(cumulative)
   incidence_data <- count_data |>
       left_join(pop_data, by = joining_var) |>
