0c89d5a048336e8fe7bf290779bcee3f56ee4495
Alejandra Martinez Sanchez <alejama@student.sv.uio.no>
2024-09-23 13:32:31 +0200

    add time flag option and suppression

--- R/calculate_prev.R
+++ R/calculate_prev.R
@@ -1,12 +1,18 @@
 #' Calculate prevalence rates
 #'
+#' @description
+#'The `calculate_prev()` function calculates prevalence rates based on the given diagnostic and demographic information.
+#'Prevalence represents the number of cases of a given diagnosis that exist in a population of interest at a specified point or period in time.
+#'
 #' @param linked_data Dataset containing relevant diagnostic and demographic information
 #' @param id_col Name (character) of the ID column in the data set (unique personal identifier). Default is "id".
 #' @param pop_data Dataset containing relevant population information.
 #' @param pop_col Name (character) of the column containing population counts in the population dataset.
-#' @param time_period ?? Optional
+#' @param time_p Optional time period or time point. Useful to calculate either point or period prevalence.
 #' @param grouping_vars Optional character vector including grouping variables for the aggregation of diagnostic counts (eg. sex, education).
 #' @param only_counts Return only diagnostic count, instead of prevalence rates. Default is set to FALSE.
+#' @param suppression Apply suppression to results (intermediate and rates) in order to maintain statistical confidentiality.
+#' @param suppression_treshold Threshold for suppression, default is set to 5 (NPR standard).
 #' @return Prevalence rate table
 #' @importFrom rlang sym
 #' @importFrom dplyr group_by
@@ -14,6 +20,7 @@
 #' @importFrom dplyr n_distinct
 #' @importFrom dplyr n
 #' @importFrom dplyr left_join
+#' @importFrom dplyr filter
 #' @importFrom dplyr mutate
 #'
 #' @export
@@ -21,13 +28,16 @@
 
 calculate_prev <- function(linked_data,
                            id_col = "id",
+                           date_col = "date",
                            pop_data,
                            pop_col = "pop_count",
-                           time_period = NULL,
+                           time_p = NULL,
                            grouping_vars = NULL,
-                           only_counts = FALSE){
+                           only_counts = FALSE,
+                           suppression = TRUE,
+                           suppression_treshold = 5){
 
-  ##Input validation ####
+  ## Input validation ####
   stopifnot("Requires linked and population dataset"= !is.null(linked_data), !is.null(pop_data))
 
   if(!all(grouping_vars %in% names(linked_data))) {
@@ -38,8 +48,19 @@ calculate_prev <- function(linked_data,
     stop("Your data must contain the specified 'id' column.")
   }
 
-  ###Filter for time period? Or how to establish that it is period prevalence instead of point prevalence? ####
+  ##### If time_p is specified, filter data ####
+  if (!is.null(time_p)) {
+    linked_data <- linked_data |>
+      dplyr::filter(.data[[date_col]] >= time_p[1],
+             .data[[date_col]] <= time_p[2])
+  }
 
+  #### Suppression helper function ####
+
+  suppress_values <- function(data, columns, threshold) {
+    data <- data |>
+      mutate(across(all_of(columns), ~ ifelse(. <= threshold, NA, .)))
+  }
 
   ##Group by specified grouping variables ####
   if (!is.null(grouping_vars)) {
@@ -49,18 +70,26 @@ calculate_prev <- function(linked_data,
     data_grouped <- linked_data
   }
 
-  ##Calculate rates ####
+  ##Calculate counts ####
   id_col_sym <- rlang::sym(id_col)
   count_data <- data_grouped |>
     dplyr::summarise(unique_id = dplyr::n_distinct(!!id_col_sym),
                      total_events = dplyr::n(), .groups = 'drop')
 
+  ##Intermediate results and suppression ####
   if (only_counts){
-    return(count_data)
+    if (suppression){
+      count_data_suppressed <- suppress_values(data = count_data, columns = c("unique_id", "total_events"), threshold = suppression_treshold)
+    } else {
+      warning("No suppression. Confidentiality cannot be assured.")
+      count_data_suppressed <- count_data
+    }
+    return(count_data_suppressed)
   }
 
   ##Join with population and calculate rates ####
   prevalence <- count_data |>
       dplyr::left_join(pop_data, by = grouping_vars) |>
       dplyr::mutate(prev_rate = unique_id/.data[[pop_col]])
+  return(prevalence)
 }
