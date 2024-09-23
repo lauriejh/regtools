066c3094df079a71238d1dfd67a5925c73d05447
Alejandra Martinez Sanchez <alejama@student.sv.uio.no>
2024-09-23 13:54:41 +0200

    updated imports

--- R/calculate_incidence.R
+++ R/calculate_incidence.R
@@ -1,5 +1,5 @@
 calculate_incidence <- function(count_data, pop_data, joining_var, diag_count_col = "diag_count", pop_count_col = "pop_count"){
   incidence_data <- count_data |>
-      left_join(pop_data, by = joining_var) |>
-      mutate(prev_rate = diag_count_col/pop_count_col)
+    dplyr::left_join(pop_data, by = joining_var) |>
+    dplyr::mutate(prev_rate = diag_count_col/pop_count_col)
 }
