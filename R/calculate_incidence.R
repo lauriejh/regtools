2c77bc76b190ce5afbcb29e5e6f27d9605a6981a
Alejandra Martinez Sanchez <alejama@student.sv.uio.no>
2024-09-30 13:20:45 +0200

    add notes for incidence

--- R/calculate_incidence.R
+++ R/calculate_incidence.R
@@ -1,5 +1,16 @@
-calculate_incidence <- function(count_data, pop_data, joining_var, diag_count_col = "diag_count", pop_count_col = "pop_count"){
-  incidence_data <- count_data |>
+calculate_incidence <- function(count_data,
+                                pop_data,
+                                joining_var,
+                                diag_count_col = "diag_count",
+                                pop_count_col = "pop_count"){
+  #Necessary that it is only the first occurrence for incidence rates (new cases)
+
+  #For cumulative incidence:
+  #new cases in a period/ population at risk at start of the period (only diseased free population)
+
+  #For incidence rate:
+  #number of new diagnoses/total person-time at risk (need to account for left-truncation and censoring etc...)
+   incidence_data <- count_data |>
     dplyr::left_join(pop_data, by = joining_var) |>
     dplyr::mutate(prev_rate = diag_count_col/pop_count_col)
 }
