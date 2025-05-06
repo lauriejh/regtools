2c835d593d7551be7355bfc8225ddb8c9d75c8c3
amslala <alejama@uio.no>
2025-05-06 15:33:28 +0200

    coord flip and improved colors

--- R/plot_rates.R
+++ R/plot_rates.R
@@ -14,6 +14,11 @@
 #' @param plot_title
 #' @param start_end_points
 #' @param interactive
+#' @param single_color
+#' @param x_name
+#' @param y_name
+#' @param legend_title
+#' @param coord_flip
 #'
 #' @returns
 #' @export
@@ -24,10 +29,19 @@
 plot_rates <- function(data, date_col, rate_col, grouping_var = NULL, facet_var = NULL,
                      plot_type = c("line", "bar_chart", "lollipop", "jitter"),
                      percent = TRUE, palette = c("fhi_colors", "viridis", "okabe_ito"),
+                     single_color = "black",
                      annotate_line = NULL, CI_lower = NULL, CI_upper = NULL,
-                     plot_title, start_end_points = F, interactive = FALSE){
+                     plot_title = "", x_name = "", y_name = "", legend_title = "", coord_flip = F, start_end_points = F, interactive = FALSE){
 
 
+  # Input validation   ---------------------------------------------------------
+
+  type <- match.arg(plot_type, several.ok = TRUE)
+
+  palette_type <- match.arg(palette, several.ok = FALSE)
+
+
+  # Set theme   ---------------------------------------------------------------
   regtools_theme <- ggthemes::theme_hc() +
     theme(
       strip.background = element_blank(),
@@ -46,30 +60,46 @@ plot_rates <- function(data, date_col, rate_col, grouping_var = NULL, facet_var 
     )
 
 
-  type <- match.arg(plot_type, several.ok = FALSE) #later change to true, and return all plots with patchwork
 
-  palette_type <- match.arg(palette, several.ok = FALSE) #later change to true, and return all plots with patchwork
+  # Base plots  ---------------------------------------------------------------
 
-  # make the base plot for line, lollipop and jitter
-  base_plot <- ggplot(data, aes(x = {{date_col}}, y = {{rate_col}},
-                        colour = {{grouping_var}},
-                        group  = {{grouping_var}}))
+  if (!is.null(grouping_var)){
+    base_plot <- ggplot(data, aes(x = !!rlang::sym(date_col), y = !!rlang::sym(rate_col),
+                                  colour = !!rlang::sym(grouping_var),
+                                  group  = !!rlang::sym(grouping_var)))
 
-  # Select plot  ---------------------------------------------------------------
 
   plot <- switch(type,
 
                  line = base_plot + geom_line(size = 1.2) + geom_point(size = 1.5),
 
-                 bar_chart = ggplot(data, aes(x = {{date_col}}, y = {{rate_col}}, fill = {{grouping_var}}))+ geom_bar(position = "dodge", stat = "identity"),
+                 bar_chart = ggplot(data, aes(x = !!rlang::sym(date_col), y = !!rlang::sym(rate_col), fill = !!rlang::sym(grouping_var)))+ geom_bar(position = "dodge", stat = "identity"),
 
-                 lollipop = base_plot + geom_segment(aes(y = 0, yend = {{rate_col}}),size = 1) + geom_point(size = 3),
+                 lollipop = base_plot + geom_segment(aes(y = 0, yend = !!rlang::sym(rate_col)),size = 1) + geom_point(size = 3),
 
                  jitter = base_plot + geom_jitter(width = .1, height = 0, alpha = .6),
 
                  stop("Unknown plot_type")
                  )
 
+
+  } else if (is.null(grouping_var)){
+    base_plot <- ggplot(data, aes(x = !!rlang::sym(date_col), y = !!rlang::sym(rate_col), group = 1))
+
+
+    plot <- switch(type,
+
+                   line = base_plot + geom_line(size = 1.2, color = single_color) + geom_point(size = 1.5),
+
+                   bar_chart = ggplot(data, aes(x = !!rlang::sym(date_col), y = !!rlang::sym(rate_col)))+ geom_bar(color = single_color, fill = single_color, position = "dodge", stat = "identity"),
+
+                   lollipop = base_plot + geom_segment(aes(y = 0, yend = !!rlang::sym(rate_col)),size = 1) + geom_point(size = 3),
+
+                   jitter = base_plot + geom_jitter(width = .1, height = 0, alpha = .6),
+
+                   stop("Unknown plot_type")
+    )
+  }
 
   if(!is.null(facet_var)){
     plot <- plot + facet_wrap(vars(!!rlang::sym(facet_var)))
@@ -82,55 +112,63 @@ plot_rates <- function(data, date_col, rate_col, grouping_var = NULL, facet_var 
 
 
 
-  # Theme  and palette ---------------------------------------------------------
+  # Theme and palette for multiple groups --------------------------------------
 
   plot <- plot +
     regtools_theme +
-    ggplot2::labs(title = plot_title)
+    ggplot2::labs(title = plot_title,
+                  x = x_name,
+                  y = y_name,
+                  color = legend_title)
 
+  plot <- switch(palette_type,
+                 fhi_colors = plot + ggplot2::scale_colour_manual(values = c("#7176c9","#a93c38","#f9dc8c","#61d2b2")) +
+                   ggplot2::scale_fill_manual(values = c("#7176c9","#a93c38","#f9dc8c","#61d2b2")),
 
-  if (plot_type %in% c("line", "lollipop", "jitter")){
-    plot <- switch(palette_type,
+                 viridis = plot + ggplot2::scale_colour_viridis_d() +
+                   ggplot2::scale_fill_viridis_d(),
 
-                   fhi_colors = plot + ggplot2::scale_colour_manual(values = c("#7176c9","#a93c38","#f9dc8c","#61d2b2")),
+                 okabe_ito = plot + ggplot2::scale_colour_manual(values = c(palette.colors(palette = "Okabe-Ito"))) +
+                   ggplot2::scale_fill_manual(values = c(palette.colors(palette = "Okabe-Ito"))),
 
-                   viridis = plot + ggplot2::scale_colour_viridis_d(),
-
-                   okabe_ito = plot + ggplot2::scale_colour_manual(values = c(palette.colors(palette = "Okabe-Ito"))),
-
-                   stop("Unknown palette type")
-    )
-  } else if (plot_type == "bar_chart"){
-    plot <- switch(palette_type,
-
-                   fhi_colors = plot + ggplot2::scale_fill_manual(values = c("#7176c9","#a93c38","#f9dc8c","#61d2b2")),
-
-                   viridis = plot + ggplot2::scale_fill_viridis_d(),
-
-                   okabe_ito = plot + ggplot2::scale_fill_manual(values = c(palette.colors(palette = "Okabe-Ito"))),
-
-                   stop("Unknown palette type")
-    )
-  }
+                 stop("Unknown palette type"))
 
 
 
   # Extra visuals -----------------------------------------------------------
 
+  if(coord_flip && plot_type %in% c("bar_chart", "lollipop")){
+    plot <- plot + ggplot2::coord_flip() + theme(panel.grid.major.x = ggplot2::element_line(color = "#D8D8D8"),
+                                                 panel.grid.major.y = element_blank())
+  }
+
   if (!is.null(CI_upper) && !is.null(CI_lower)){
-    plot <- plot + ggplot2::geom_ribbon(aes(ymin=!!rlang::sym(CI_lower), ymax=!!rlang::sym(CI_upper), fill={{grouping_var}}),alpha=0.2, linetype = 0, show.legend = FALSE)
+    if(plot_type != "bar_chart"){
+      if(!is.null(grouping_var)){
+        plot <- plot + ggplot2::geom_ribbon(aes(ymin=!!rlang::sym(CI_lower), ymax=!!rlang::sym(CI_upper), fill = !!rlang::sym(grouping_var)),alpha=0.1, linetype = 0, show.legend = FALSE)
+      } else if (is.null(grouping_var)){
+        plot <- plot + ggplot2::geom_ribbon(aes(ymin=!!rlang::sym(CI_lower), ymax=!!rlang::sym(CI_upper)), fill = single_color, alpha=0.1, linetype = 0, show.legend = FALSE)
+      }
+    } else if(plot_type == "bar_chart"){
+      if(!is.null(grouping_var)){
+        plot <- plot + ggplot2::geom_errorbar(aes(ymin=!!rlang::sym(CI_lower), ymax=!!rlang::sym(CI_upper)), position =  ggplot2::position_dodge(0.9), width = 0.2)
+      } else if (is.null(grouping_var)){
+        plot <- plot + ggplot2::geom_errorbar(aes(ymin=!!rlang::sym(CI_lower), ymax=!!rlang::sym(CI_upper)))
+      }
+    }
   }
 
   if(!is.null(annotate_line)){
-    plot <- plot + ggplot2::geom_vline(aes(xintercept = as.character(annotate_line)))
+    plot <- plot + ggplot2::geom_vline(aes(xintercept = as.character(annotate_line)), linetype="dashed", size = 0.6)
   }
 
-  if(start_end_points == T){
-    data_start_end_points <-test_prev_series |>
-      dplyr::group_by(innvandringsgrunn, sex) |>
+  if(start_end_points){
+    variables <- c(grouping_var, facet_var)
+    data_start_end_points <- data |>
+      dplyr::group_by(dplyr::across(tidyselect::all_of(variables))) |>
       dplyr::slice(1, dplyr::n())
 
-    plot <- plot + ggrepel::geom_text_repel(data = data_start_end_points, aes(label = scales::percent({{rate_col}}, accuracy = 0.01)), color = "black", size = 4.7)
+    plot <- plot + ggrepel::geom_text_repel(data = data_start_end_points, aes(label = scales::percent(!!rlang::sym(rate_col), accuracy = 0.01)), color = "black", size = 4.7)
 
   }
 
