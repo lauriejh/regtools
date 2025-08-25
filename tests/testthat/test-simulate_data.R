ebec9e7df7fa8dbfe983b663a1fbeae5a448305c
amslala <alejama@uio.no>
2025-08-25 22:27:01 +0200

    test for simulate data: input validation, diagnostic codes, years of birth

--- tests/testthat/test-simulate_data.R
+++ tests/testthat/test-simulate_data.R
@@ -1,3 +1,307 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+
+test_that("Input validation works", {
+
+  expect_error(simulate_data(
+    population_size = 1000,
+    prefix_ids = "P000",
+    length_ids = 6,
+    family_codes = c("F45", "F84"),
+    pattern = "equal",
+    prevalence = 0.023,
+    diag_years  = c(2012:2020),
+    sex_vector = c(0, 1),
+    y_birth = c(2010:2018),
+    filler_codes = "F",
+    filler_y_birth = c(2000:2009),
+    unvarying_codes = list("innvandringsgrunn" = c("ARB", "NRD", "UKJ")),
+    unvarying_codes_filler = list("innvandringsgrunn" = c("FAMM", "UTD")),
+    varying_query = "fylke"
+    )
+  )
+
+
+  expect_error(simulate_data(
+    population_size = 1000,
+    prefix_ids = "P000",
+    length_ids = 6,
+    family_codes = c("F45", "F84"),
+    pattern = "increase",
+    diag_years  = c(2012:2020),
+    sex_vector = c(0, 1),
+    y_birth = c(2010:2018),
+    filler_codes = "F",
+    filler_y_birth = c(2000:2009),
+    unvarying_codes = list("innvandringsgrunn" = c("ARB", "NRD", "UKJ")),
+    unvarying_codes_filler = list("innvandringsgrunn" = c("FAMM", "UTD")),
+    varying_query = "fylke"
+    ), "Either prevalance or incidence has to be provided"
+  )
+
+  expect_error(simulate_data(
+    population_size = 1000,
+    prefix_ids = "P000",
+    length_ids = 6,
+    family_codes = c("F45", "F84"),
+    pattern = "increase",
+    prevalence = 23,
+    diag_years  = c(2012:2020),
+    sex_vector = c(0, 1),
+    y_birth = c(2010:2018),
+    filler_codes = "F",
+    filler_y_birth = c(2000:2009),
+    unvarying_codes = list("innvandringsgrunn" = c("ARB", "NRD", "UKJ")),
+    unvarying_codes_filler = list("innvandringsgrunn" = c("FAMM", "UTD")),
+    varying_query = "fylke"
+  ), "Prevalence rate is not expressed as a proportion between 0 and 1."
+  )
+
+  expect_error(simulate_data(
+    population_size = 1000,
+    prefix_ids = "P000",
+    length_ids = 6,
+    family_codes = c("F45", "F84"),
+    pattern = "increase",
+    prevalence = .023,
+    diag_years  = c(2012:2020),
+    sex_vector = c(0, 1),
+    y_birth = c(2010:2018),
+    filler_codes = "F",
+    filler_y_birth = c(2000:2009),
+    varying_query = "fylke"), "Either unvarying queries for SSB's API, or unvarying codes need to be provided")
+
+  expect_error(simulate_data(
+    population_size = 1000,
+    prefix_ids = "P000",
+    length_ids = 6,
+    family_codes = c("F45", "F84"),
+    pattern = "increase",
+    prevalence = .023,
+    diag_years  = c(2012:2020),
+    sex_vector = c(0, 1),
+    y_birth = c(2010:2018),
+    filler_codes = "F",
+    filler_y_birth = c(2000:2009),
+    unvarying_codes = list("innvandringsgrunn" = c("ARB", "NRD", "UKJ")),
+    unvarying_codes_filler = list("innvandringsgrunn" = c("FAMM", "UTD")),
+    varying_codes = c("01", "03")),
+    "You have provided varying codes. Filler varying codes also need to be provided."
+    )
+
+  expect_message(simulate_data(
+    population_size = 1000,
+    prefix_ids = "P000",
+    length_ids = 6,
+    family_codes = c("F45", "F84"),
+    pattern = "increase",
+    prevalence = .023,
+    diag_years  = c(2012:2020),
+    sex_vector = c(0, 1),
+    y_birth = c(2010:2018),
+    filler_codes = "F",
+    filler_y_birth = c(2000:2009),
+    unvarying_codes = list("innvandringsgrunn" = c("ARB", "NRD", "UKJ")),
+    unvarying_codes_filler = list("innvandringsgrunn" = c("FAMM", "UTD")),
+  ), "Varying query and varying codes arguments are empty. The varying dataset will not be generated.")
+
+expect_error(simulate_data(
+  population_size = 1000,
+  prefix_ids = "P000",
+  length_ids = 6,
+  family_codes = c("F45", "F84"),
+  pattern = "increase",
+  incidence = c(0.01, 0.02),
+  diag_years  = c(2012:2020),
+  sex_vector = c(0, 1),
+  y_birth = c(2010:2018),
+  filler_codes = "F",
+  filler_y_birth = c(2000:2009),
+  unvarying_codes = list("innvandringsgrunn" = c("ARB", "NRD", "UKJ")),
+  unvarying_codes_filler = list("innvandringsgrunn" = c("FAMM", "UTD")),
+  varying_query = "fylke"
+  ), "Number of elements in incidence vector do not correspond with number of diagnosis years"
+)
+
+expect_error(simulate_data(
+  population_size = 1000,
+  prefix_ids = "P000",
+  length_ids = 6,
+  family_codes = c("F45", "F84"),
+  pattern = "increase",
+  incidence = c(0.01, 2),
+  diag_years  = c(2012,2013),
+  sex_vector = c(0, 1),
+  y_birth = c(2010:2018),
+  filler_codes = "F",
+  filler_y_birth = c(2000:2009),
+  unvarying_codes = list("innvandringsgrunn" = c("ARB", "NRD", "UKJ")),
+  unvarying_codes_filler = list("innvandringsgrunn" = c("FAMM", "UTD")),
+  varying_query = "fylke"))
+
+expect_error(simulate_data(
+  population_size = 1000,
+  prefix_ids = "P000",
+  length_ids = 6,
+  family_codes = c("F45", "F84"),
+  pattern = "increase",
+  incidence = c(0.01, 2),
+  diag_years  = c(2012,2013),
+  sex_vector = c(0, 1),
+  y_birth = c(2010:2018),
+  filler_codes = "F",
+  filler_y_birth = c(2000:2009),
+  unvarying_codes = list("innvandringsgrunn" = c("ARB", "NRD", "UKJ")),
+  unvarying_codes_filler = list("innvandringsgrunn" = c("FAMM", "UTD")),
+  varying_query = "fylke"), "Not all incidence rates are expressed as a proportion between 0 and 1.")
+
+expect_error(simulate_data(
+  population_size = 100,
+  prefix_ids = "P000",
+  length_ids = 6,
+  family_codes = c("F45", "F84"),
+  diag_code_type = "NPR",
+  pattern = "random",
+  prevalence = 0.02,
+  diag_years  = c(2012:2020),
+  sex_vector = c(0, 1),
+  y_birth = c(2010:2018),
+  filler_codes = "F",
+  filler_y_birth = c(2000:2009),
+  unvarying_codes = list("innvandringsgrunn" = c("ARB", "NRD", "UKJ")),
+  unvarying_codes_filler = list("innvandringsgrunn" = c("FAMM", "UTD")),
+  varying_query = "fylke"
+  ), "diag_code_type not valid. Must be either 'icd' or 'icpc'.")
+
+
 })
+
+
+
+ #Test that population size matches with datasets and with diag years
+
+
+test_that("Correct population size", {
+  n_pop <- 100
+  diag_years <- c(2012:2020)
+  test_simulate <- simulate_data(
+    population_size = n_pop,
+    prefix_ids = "P000",
+    length_ids = 6,
+    family_codes = c("F45", "F84"),
+    pattern = "random",
+    prevalence = 0.02,
+    diag_years  = diag_years,
+    sex_vector = c(0, 1),
+    y_birth = c(2010:2018),
+    filler_codes = "F",
+    filler_y_birth = c(2000:2009),
+    unvarying_codes = list("innvandringsgrunn" = c("ARB", "NRD", "UKJ")),
+    unvarying_codes_filler = list("innvandringsgrunn" = c("FAMM", "UTD")),
+    varying_query = "fylke"
+  )
+  expect_equal(nrow(test_simulate$invar_df), n_pop)
+  expect_equal(nrow(test_simulate$var_df), n_pop*length(diag_years))
+  expect_equal(test_simulate$diag_df |> dplyr::distinct(id) |> nrow(), n_pop)
+})
+
+
+# Test that diag codes match (use other functions in package)
+
+test_that("Diagnostic codes", {
+  icd_simulate <- simulate_data(
+    population_size = 100,
+    prefix_ids = "P000",
+    length_ids = 6,
+    family_codes = c("F45", "F84"),
+    diag_code_type = "icd",
+    pattern = "random",
+    prevalence = 0.02,
+    diag_years  = c(2012:2020),
+    sex_vector = c(0, 1),
+    y_birth = c(2010:2018),
+    filler_codes = "F",
+    filler_y_birth = c(2000:2009),
+    unvarying_codes = list("innvandringsgrunn" = c("ARB", "NRD", "UKJ")),
+    unvarying_codes_filler = list("innvandringsgrunn" = c("FAMM", "UTD")),
+    varying_query = "fylke"
+  )
+
+  df <- icd_simulate$diag_df
+
+  expect_gt(length(stringr::str_starts(df$code, "F")), 0)
+
+
+  icpc_simulate <- simulate_data(
+    population_size = 100,
+    prefix_ids = "P000",
+    length_ids = 6,
+    family_codes = c("P"),
+    diag_code_type = "icpc",
+    pattern = "random",
+    prevalence = 0.02,
+    diag_years  = c(2012:2020),
+    sex_vector = c(0, 1),
+    y_birth = c(2010:2018),
+    filler_codes = "A",
+    filler_y_birth = c(2000:2009),
+    unvarying_codes = list("innvandringsgrunn" = c("ARB", "NRD", "UKJ")),
+    unvarying_codes_filler = list("innvandringsgrunn" = c("FAMM", "UTD")),
+    varying_query = "fylke"
+  )
+
+  df <- icpc_simulate$diag_df
+
+  expect_gt(length(stringr::str_starts(df$code, "P")), 0)
+
+
+})
+
+
+#Test that y birth match
+
+test_that("Year of birth", {
+  years_birth <-c(2010:2018)
+  filler_years_birth <- c(2000:2009)
+
+  test <- simulate_data(
+    population_size = 100,
+    prefix_ids = "P000",
+    length_ids = 6,
+    family_codes = c("F45", "F84"),
+    diag_code_type = "icd",
+    pattern = "random",
+    prevalence = 0.02,
+    diag_years  = c(2012:2020),
+    sex_vector = c(0, 1),
+    y_birth = years_birth,
+    filler_codes = "F",
+    filler_y_birth = filler_years_birth,
+    unvarying_codes = list("innvandringsgrunn" = c("ARB", "NRD", "UKJ")),
+    unvarying_codes_filler = list("innvandringsgrunn" = c("FAMM", "UTD")),
+    varying_query = "fylke"
+  )
+
+  all_years <- test$invar_df$y_birth
+
+  expect_true(all(all_years %in% c(years_birth, filler_years_birth)))
+
+  filtered_df <- regtools::filter_diag(test$diag_df, pattern_codes = c("F45", "F84"), code_col = "code", id_col = "id")
+
+  curated_df <- regtools::curate_diag(filtered_df, min_diag = 1, first_diag = TRUE, code_col = "code", date_col = "diag_year")
+
+  invar_test <- test$invar_df
+
+  filtered_df_2 <- regtools::filter_demo(invar_test, data_type = "t_invariant", filter_param = list("innvandringsgrunn" = c("ARB", "NRD", "UKJ")), id_col = "id", rm_na = FALSE)
+
+  linked_df <- regtools::link_diag_demo(curated_df, data_demo_inv = filtered_df_2, id_col = "id")
+  expect_true(all(linked_df$y_birth %in% years_birth))
+
+})
+
+
+#Test that unvarying codes are ok (and queries)
+
+#Test that varying codes are ok (and queries)
+
+#Test that prevalence is correct
+
+#Test that incidence is correct
